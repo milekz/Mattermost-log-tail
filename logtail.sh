f87cbde4bc8d26ff2c7352d0de26d5a0384743f1
Miłosz Szewczak <milek@milek.eu>
2020-04-08 08:16:34 +0200

    Update logtail.sh

--- logtail.sh
+++ logtail.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
-
-tail -n0 -F "/var/log/file.log" | while read LINE; do
-  (echo "$LINE" | grep -e "specific text") && curl -X POST --silent --data-urlencode \
-    "payload={\"text\": \"$(echo $LINE | sed "s/\"/'/g")\"}" "(webhookURL)";
+tail -n0 -F "${1}" | while read LINE; do
+  (echo "$LINE" ) && curl -X POST --silent --data-urlencode \
+    "payload={\"text\": \"$(echo $LINE | sed "s/\"/'/g")\"}" "${2}";
 done
 
