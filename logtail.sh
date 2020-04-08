fe821604be9c220feff9c61611b21e7f12f04f35
Miłosz Szewczak <milek@milek.eu>
2020-04-08 09:15:41 +0200

    Update logtail.sh

--- logtail.sh
+++ logtail.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 tail -n0 -F "${1}" | while read LINE; do
+  ####(echo "$LINE" | grep -e "specific text") && curl -X POST --silent --data-urlencode \
   (echo "$LINE" ) && curl -X POST --silent --data-urlencode \
     "payload={\"username\": \"${3}\" , \"text\": \"$(echo $LINE | sed "s/\"/'/g")\"}" "${2}";
 done
